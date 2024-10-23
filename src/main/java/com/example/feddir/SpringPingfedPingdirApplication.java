82281ab4ca17c19a5c5740c4c2ffc0577016fb4b
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-10-23 17:07:14 +0600

    initial commit

---
+++ src/main/java/com/example/feddir/SpringPingfedPingdirApplication.java
@@ -0,0 +1,13 @@
+package com.example.feddir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPingfedPingdirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPingfedPingdirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/feddir/SpringPingfedPingdirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feddir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPingfedPingdirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
